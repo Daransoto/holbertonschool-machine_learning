49c974c199bac9d2ced113b9340bf0c4857f025a
Daransoto <901@holbertonschool.com>
2020-03-11 03:35:28 +0000

    First upload of task 1

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Defines a single neuron performing binary classification.
+"""
+import numpy as np
+
+
+class Neuron:
+    """ Neuron class. """
+    def __init__(self, nx):
+        """ Initializer for the neuron. """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ Getter for W. """
+        return self.__W
+
+    @property
+    def b(self):
+        """ Getter for b. """
+        return self.__b
+
+    @property
+    def A(self):
+        """ Getter for A. """
+        return self.__A
