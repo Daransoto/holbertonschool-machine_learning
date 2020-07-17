ba8a980328cb67cf1458a8d28f6a56b2e4aedb49
Darwin <901@holbertonschool.com>
2020-07-17 02:00:42 +0000

    Added documentation

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+"""
+This module contains the function matrix_transpose.
+"""
+
+
 def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+    Type matrix: list(list(int/float))
+    Type return: list(list(int/float))
+    """
     matrixT = []
     for i in range(len(matrix[0])):
         current = []
