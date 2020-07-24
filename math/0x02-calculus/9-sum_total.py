f3a89b118cb8d386c0944347d380b2e5d7b75eab
Darwin <901@holbertonschool.com>
2020-07-24 20:26:11 +0000

    Changed condition to include case when n = 0

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """ Calculates the summation of i squared from 1 to n.
     n is the number to stop at. """
-    if type(n) != int or n < 0:
+    if type(n) != int or n <= 0:
         return None
     return n * (n + 1) * (2*n + 1) // 6
