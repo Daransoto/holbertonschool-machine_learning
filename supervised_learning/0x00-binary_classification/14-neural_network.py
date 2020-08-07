5f485b8f3f3aace09651f72bd17699eebfb02c0f
Darwin <901@holbertonschool.com>
2020-08-07 19:20:54 +0000

    Using double quotes in raises

--- supervised_learning/0x00-binary_classification/14-neural_network.py
+++ supervised_learning/0x00-binary_classification/14-neural_network.py
@@ -87,13 +87,13 @@ class NeuralNetwork:
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """ Trains the network. """
         if type(iterations) != int:
-            raise TypeError('iterations must be an integer')
+            raise TypeError("iterations must be an integer")
         if iterations <= 0:
-            raise ValueError('iterations must be a positive integer')
+            raise ValueError("iterations must be a positive integer")
         if type(alpha) != float:
-            raise TypeError('alpha must be a float')
+            raise TypeError("alpha must be a float")
         if alpha <= 0:
-            raise ValueError('alpha must be positive')
+            raise ValueError("alpha must be positive")
         for i in range(iterations):
             self.forward_prop(X)
             self.gradient_descent(X, Y, self.A1, self.A2, alpha)
