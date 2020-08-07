fd70031d348b2c8b9b0126e1fe6ef565576d939b
Darwin <901@holbertonschool.com>
2020-08-07 19:15:00 +0000

    Using forward_prop instead of evaluate

--- supervised_learning/0x00-binary_classification/14-neural_network.py
+++ supervised_learning/0x00-binary_classification/14-neural_network.py
@@ -88,13 +88,13 @@ class NeuralNetwork:
         """ Trains the network. """
         if type(iterations) != int:
             raise TypeError('iterations must be an integer')
-        if iterations < 0:
+        if iterations <= 0:
             raise ValueError('iterations must be a positive integer')
         if type(alpha) != float:
             raise TypeError('alpha must be a float')
-        if alpha < 0:
+        if alpha <= 0:
             raise ValueError('alpha must be positive')
         for i in range(iterations):
-            self.evaluate(X, Y)
+            self.forward_prop(X)
             self.gradient_descent(X, Y, self.A1, self.A2, alpha)
         return self.evaluate(X, Y)
