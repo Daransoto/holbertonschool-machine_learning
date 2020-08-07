b06f63610ddb8f5a5e6f7acca609eb49773923f7
Darwin <901@holbertonschool.com>
2020-08-07 19:03:35 +0000

    Testing with condition < 0 for iterations and alpha in train function

--- supervised_learning/0x00-binary_classification/14-neural_network.py
+++ supervised_learning/0x00-binary_classification/14-neural_network.py
@@ -88,11 +88,11 @@ class NeuralNetwork:
         """ Trains the network. """
         if type(iterations) != int:
             raise TypeError('iterations must be an integer')
-        if iterations < 1:
+        if iterations < 0:
             raise ValueError('iterations must be a positive integer')
         if type(alpha) != float:
             raise TypeError('alpha must be a float')
-        if alpha <= 0:
+        if alpha < 0:
             raise ValueError('alpha must be positive')
         for i in range(iterations):
             self.evaluate(X, Y)
