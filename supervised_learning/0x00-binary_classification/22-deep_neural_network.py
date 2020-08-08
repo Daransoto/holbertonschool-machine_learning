b44f20efd9918d3bf929cfdb6f68c08647249cef
Darwin <901@holbertonschool.com>
2020-08-08 21:14:56 +0000

    Testing with different init function

--- supervised_learning/0x00-binary_classification/22-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/22-deep_neural_network.py
@@ -7,28 +7,34 @@ class DeepNeuralNetwork:
     """ Deep neural network class. """
 
     def __init__(self, nx, layers):
-        """ Initializer for the deep neural network. """
+        """ Init method """
         if type(nx) != int:
             raise TypeError('nx must be an integer')
-        if nx < 1:
-            raise ValueError('nx must be a positive integer')
-        if type(layers) != list or not layers:
+        else:
+            if nx < 1:
+                raise ValueError('nx must be a positive integer')
+
+        if type(layers) != list:
             raise TypeError('layers must be a list of positive integers')
-        self.__L = 0
+        else:
+            if any(list(map(lambda x: x <= 0, layers))):
+                raise TypeError('layers must be a list of positive integers')
+            if len(layers) < 1:
+                raise TypeError('layers must be a list of positive integers')
+        self.__L = len(layers)
         self.__cache = {}
         self.__weights = {}
-        rand = np.random.randn
-        for idx, neurons in enumerate(layers):
-            if type(neurons) != int or neurons <= 0:
-                raise TypeError('layers must be a list of positive integers')
-            if idx == 0:
-                self.weights['W1'] = rand(neurons, nx) * np.sqrt(2 / nx)
+        for i in range(len(layers)):
+            key = "W{}".format(i + 1)
+            if i == 0:
+                self.weights[key] = np.random.randn(layers[i],
+                                                    nx)*np.sqrt(2/nx)
+                self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
             else:
-                p = layers[idx - 1]
-                r = rand(neurons, p)
-                self.weights["W{}".format(idx + 1)] = r * np.sqrt(2 / p)
-            self.__L += 1
-            self.weights["b{}".format(idx + 1)] = np.zeros((neurons, 1))
+                self.weights[key] = (np.random.randn(layers[i],
+                                                     layers[i - 1]) *
+                                     np.sqrt(2/layers[i - 1]))
+                self.weights["b{}".format(i + 1)] = np.zeros((layers[i], 1))
 
     @property
     def L(self):
@@ -46,17 +52,15 @@ class DeepNeuralNetwork:
         return self.__weights
 
     def forward_prop(self, X):
-        """ Method fo forward propagation """
+        """ Forward propagation of the network. """
         self.__cache['A0'] = X
         for i in range(self.L):
-            keyA = "A{}".format(i + 1)
-            keyb = "b{}".format(i + 1)
-            keyAo = "A{}".format(i)
-            keyW = "W{}".format(i + 1)
-            self.__cache[keyA] = 1.0 / (1.0 +
-                                        np.exp(-(np.matmul(self.weights[keyW],
-                                                           self.cache[keyAo])
-                                               + self.weights[keyb])))
+            w = self.weights['W{}'.format(i + 1)]
+            b = self.weights['b{}'.format(i + 1)]
+            p_a = self.cache['A' + str(i)]
+            A = 1 / (1 + np.exp(-(np.matmul(w, p_a) + b)))
+            self.__cache["A" + str(i + 1)] = A
+        return (self.__cache["A" + str(i)], self.cache)
 
     def cost(self, Y, A):
         """ Calculates the cost of the network. """
