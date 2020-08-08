6eb6dee68820d91e3d543e4019785129a80bd31e
Darwin <901@holbertonschool.com>
2020-08-08 21:01:15 +0000

    Testing different train function

--- supervised_learning/0x00-binary_classification/22-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/22-deep_neural_network.py
@@ -85,17 +85,22 @@ class DeepNeuralNetwork:
             self.__weights["W" + str(i)] = w - alpha * dw
             self.__weights["b" + str(i)] = b - alpha * db
 
-    def train(self, X, Y, iterations=5000, alpha=0.05):
-        """ Trains the network. """
+def train(self, X, Y, iterations=5000, alpha=0.05):
+        """ Method to train the neuron """
         if type(iterations) != int:
             raise TypeError('iterations must be an integer')
-        if iterations < 1:
-            raise ValueError('iterations must be a positive integer')
+        else:
+            if iterations < 0:
+                raise ValueError('iterations must be a positive integer')
+
         if type(alpha) != float:
             raise TypeError('alpha must be a float')
-        if alpha <= 0:
-            raise ValueError('alpha must be positive')
+        else:
+            if alpha < 0:
+                raise ValueError('alpha must be positive')
+
         for i in range(iterations):
-            self.forward_prop(X)
-            self.gradient_descent(Y, self.cache, alpha)
+            A, cache = self.forward_prop(X)
+            self.gradient_descent(Y, cache, alpha)
+
         return self.evaluate(X, Y)
