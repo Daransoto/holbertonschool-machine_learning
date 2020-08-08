0aa5c8545e0421483889a7cda4bae6036d3f3b92
Darwin <901@holbertonschool.com>
2020-08-08 17:02:12 +0000

    Added checks for layers list and fixed lowercase letter

--- supervised_learning/0x00-binary_classification/17-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -5,27 +5,28 @@ import numpy as np
 
 class DeepNeuralNetwork:
     """ Deep neural network class. """
+
     def __init__(self, nx, layers):
         """ Initializer for the deep neural network. """
         if type(nx) != int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if type(layers) != list:
+        if type(layers) != list or not layers:
             raise TypeError('layers must be a list of positive integers')
         self.__L = 0
         self.__cache = {}
         self.__weights = {}
         rand = np.random.randn
         for idx, neurons in enumerate(layers):
-            if type(neurons) != int:
+            if type(neurons) != int or neurons <= 0:
                 raise TypeError('layers must be a list of positive integers')
             if idx == 0:
-                self.__weights['w1'] = rand(neurons, nx) * np.sqrt(2 / nx)
+                self.__weights['W1'] = rand(neurons, nx) * np.sqrt(2 / nx)
             else:
                 p = layers[idx - 1]
                 r = rand(neurons, p)
-                self.__weights["w{}".format(idx + 1)] = r * np.sqrt(2 / p)
+                self.__weights["W{}".format(idx + 1)] = r * np.sqrt(2 / p)
             self.__L += 1
             self.__weights["b{}".format(idx + 1)] = np.zeros((neurons, 1))
 
