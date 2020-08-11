974e2d933a662c862309611207d4ed45d10e7ff7
Darwin <901@holbertonschool.com>
2020-08-11 18:10:33 +0000

    Fixed map function arguments

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,7 +7,7 @@ def one_hot_encode(Y, classes):
     """ one hot encoding of Y array with classes amount of classes. """
     if type(Y) != np.ndarray or type(classes) != int or classes <= 0\
        or classes < np.max(Y) or\
-       any(map(Y, lambda x: type(x) != int or x < 0)):
+       any(map(lambda x: type(x) != int or x < 0, Y)):
         return None
     ans = np.eye(classes)[Y]
     return ans.T
