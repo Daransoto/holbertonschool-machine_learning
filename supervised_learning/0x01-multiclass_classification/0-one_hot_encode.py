0f22692037242ec53916c4f4981b4736160445f5
Darwin <901@holbertonschool.com>
2020-08-11 18:23:20 +0000

    removed check for int

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,8 +6,7 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """ one hot encoding of Y array with classes amount of classes. """
     if type(Y) != np.ndarray or type(classes) != int or classes <= 0\
-       or classes < np.max(Y) or\
-       any(map(lambda x: type(x) != int or x < 0, Y)):
+       or classes < np.max(Y) or any(map(lambda x: x < 0, Y)):
         return None
     ans = np.eye(classes)[Y]
     return ans.T
