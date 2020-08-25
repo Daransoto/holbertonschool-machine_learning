6462352addb651c66574d4b5a7340bd26c1dbb8c
Daransoto <901@holbertonschool.com>
2020-08-24 20:16:01 -0500

    First upload of task 0

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains the function create_confusion_matrix."""
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """Creates a confusion matrix"""
+    return np.dot(labels.T, logits)
